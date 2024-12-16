37cca207f43c4bd3959f84676a9c2f38b99a5084
Branden Alder <brandenalder121@gmail.com>
2024-12-16 11:10:38 -0500

    reaction controller updated. comment controller added.

--- project01/src/main/java/com/revatureproject01/project01/controller/CommentController.java
+++ project01/src/main/java/com/revatureproject01/project01/controller/CommentController.java
@@ -1,8 +1,14 @@
 package com.revatureproject01.project01.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revatureproject01.project01.entity.Comment;
 import com.revatureproject01.project01.service.CommentService;
 
 @RestController
@@ -13,5 +19,23 @@ public class CommentController {
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
     }
+
+    // TODO - get comments on a post
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity getCommentsForPost(@PathVariable Integer postId) {
+        List<Comment> comments = commentService.getCommentsByPostId(postId);
+        return ResponseEntity.status(200).body(comments);
+    }
+
+    // TODO - add a comment
+    /*
+     * @PostMapping("/posts/{postId}/comments")
+     * public ResponseEntity createComment(@RequestBody Comment comment) {
+     * // Comment comment = commentService.addComment(comment)
+     * }
+     */
+    // TODO - delete a comment
+
+    // UPDATE A COMMENT
 
 }
---
+++ project01/src/main/java/com/revatureproject01/project01/controller/ReactionController.java
@@ -0,0 +1,56 @@
+package com.revatureproject01.project01.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revatureproject01.project01.entity.Like;
+import com.revatureproject01.project01.service.ReactionService;
+
+@RestController
+public class ReactionController {
+    ReactionService reactionService;
+
+    public ReactionController(ReactionService reactionService) {
+        this.reactionService = reactionService;
+    }
+
+    // TODO - get reactions on post
+    @RequestMapping("/posts/{postId}/likes")
+    public ResponseEntity getReactionsOnPost(@PathVariable Integer postId) {
+        List<Like> likes = reactionService.getReactionsByPostId(postId);
+        return ResponseEntity.status(200).body(likes);
+    }
+
+    // todo - add reaction
+    @PostMapping("/posts/{postId}/likes")
+    public ResponseEntity createReaction(@RequestBody Like like) {
+        Like liked = reactionService.addReaction(like);
+        return ResponseEntity.status(200).body(liked);
+    }
+
+    // delete reaction
+    @DeleteMapping("/posts/{postId}/likes/{likeId}")
+    public ResponseEntity deleteReaction(@RequestBody Like like) {
+        int rows = reactionService.removeReaction(like);
+        if (rows > 0) {
+            return ResponseEntity.status(200).body(rows);
+        } else {
+            return ResponseEntity.status(200).body("");
+        }
+    }
+
+    // update reaction
+    @PatchMapping("/posts/{postId}/likes/{likeId}")
+    public ResponseEntity updateReaction(@RequestBody Like like) {
+        Like updatedLike = reactionService.updateReaction(like);
+        return ResponseEntity.status(200).body(updatedLike);
+    }
+}
